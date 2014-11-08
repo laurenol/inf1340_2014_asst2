21a4f4921aa52e5f1e2cb9de944d3c1421b8a6f4
laurenol <lauren.olar@gmail.com>
2014-11-08 18:02:06 -0500

    Minor edits

--- test_papers.py
+++ test_papers.py
@@ -5,15 +5,11 @@
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 
-__copyright__ = "2014 Susan Sim"
-__license__ = "MIT License"
-
 __status__ = "Prototype"
 
 # imports one per line
 import pytest
 from papers import decide
-
 
 def test_basic():
     assert decide("test_returning_citizen.json", "watchlist.json", "countries.json") == ["Accept", "Accept"]
