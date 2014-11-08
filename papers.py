7955c317f639beec36890e49bd17ab03118ed53e
laurenol <lauren.olar@gmail.com>
2014-11-08 15:02:38 -0500

    Made checks into appropriate functions

--- papers.py
+++ papers.py
@@ -1,6 +1,5 @@
 import re
 import datetime
-
 import json
 
 
@@ -31,94 +30,36 @@ def decide(input_file, watchlist_file, countries_file):
 
     decisions = []
 
-
     for entries in json_input_contents:
         must_quarantine = False
         must_secondary = False
         must_reject = False
         must_accept = False
 
-        from_country = entries.get("from").get("country")
+
         home_country = entries.get("home").get("country")
-
-        #Checking Completeness
-        valid = True
-
-        if(not valid_passport_format(entries.get("passport"))):
-            valid = False
-
-        for locations in entries.get("home"):
-             if entries.get("home")[locations] == "":
-                valid = False
-        for places in entries.get("from"):
-             if entries.get("from")[places] == "":
-                valid = False
-        for key in entries:
-             if entries[key] == "":
-                valid = False
-
-        if valid == False:
-           must_reject = True
-
-        #Medical Check
-
-
-        if countries_contents_json[from_country].get("medical_advisory") != "":
-            must_quarantine = True
-
-        else:
-            if(entries.get("via") != None):
-                via_country = entries.get("via").get("country")
-
-                if countries_contents_json[via_country].get("medical_advisory") != "":
-                    must_quarantine = True
-
-
 
         if home_country == "KAN":
             must_accept = True
 
+        #Checking Completeness
+        #Medical Check
         #Check for valid visas
-        entry_reason = entries.get("entry_reason")
-
-        if entry_reason == "visit" or entry_reason == "transit":
-            home_country = entries.get("home").get("country")
-
-            if entry_reason == "visit" and countries_contents_json[home_country].get("visitor_visa_required") == "1":
-                if(entries.get("visa") == None):
-                    must_reject = True
-                else:
-                    end_date = entries.get("visa").get("date")
-                    if valid_date_format(end_date):
-                        if not valid_date_check(end_date):
-                            must_reject = True
-                    else:
-                        return["Reject"]
-
-            if entry_reason =="transit" and countries_contents_json[home_country].get("transit_visa_required")== "1":
-                if(entries.get("visa") == None):
-                    must_reject = True
-                else:
-                    if valid_date_format(entries.get("visa").get("date")):
-                        end_date = entries.get("visa").get("date")
-
-                        if not valid_date_check(end_date):
-                            must_reject = True
-                    else:
-                        must_reject = True
-
         #Watchlist Check
         passport_check = entries.get("passport")
         first_name_check = entries.get("first_name")
         last_name_check = entries.get("last_name")
 
-        for person in watchlist_contents_json:
-            if person.get("passport") == passport_check.upper():
-                must_secondary = True
-
-            elif person.get("first_name") == first_name_check.upper()\
-                    and person.get("last_name") == last_name_check.upper():
-                must_secondary = True
+        if check_quarantine(entries, countries_contents_json):
+            must_quarantine = True
+        elif check_record_completeness(entries):
+            must_reject = True
+        elif valid_visa_check(entries, countries_contents_json):
+            must_reject = True
+        elif watchlist_check(passport_check,first_name_check,last_name_check,watchlist_contents_json):
+            must_secondary = True
+        else:
+            must_accept = True
 
         if must_quarantine:
             decisions.append("Quarantine")
@@ -131,8 +72,111 @@ def decide(input_file, watchlist_file, countries_file):
 
     return decisions
 
+def watchlist_check(passport, first_name, last_name, watchlist_contents):
+    """
+
+    :param passport:
+    :param first_name:
+    :param last_name:
+    :param watchlist_contents:
+    :return: return true if on list, false if not
+    """
+    for person in watchlist_contents:
+        if person.get("passport") == passport.upper():
+            return True
+
+        elif person.get("first_name") == first_name.upper()\
+            and person.get("last_name") == last_name.upper():
+
+            return True
+
+    return False
 
 
+def valid_visa_check(entry, countries_contents):
+    """
+
+    :param entry:
+    :param countries_contents:
+    :return: return true if needs to be rejected, false otherwise
+    """
+    entry_reason = entry.get("entry_reason")
+    home_country = entry.get("home").get("country")
+
+    if entry_reason == "visit" or entry_reason == "transit":
+
+        if entry_reason == "visit" and countries_contents[home_country].get("visitor_visa_required") == "1":
+            if entry.get("visa") is None:
+                return True
+            else:
+                end_date = entry.get("visa").get("date")
+                if valid_date_format(end_date):
+                    if not valid_date_check(end_date):
+                        return True
+                else:
+                    return["Reject"]
+
+        if entry_reason =="transit" and countries_contents[home_country].get("transit_visa_required")== "1":
+            if entry.get("visa") is None :
+                return True
+            else:
+                if valid_date_format(entry.get("visa").get("date")):
+                    end_date = entry.get("visa").get("date")
+
+                    if not valid_date_check(end_date):
+                        return True
+                else:
+                    return True
+    return False
+
+
+def check_quarantine(entry, countries_contents):
+    """
+
+    :param from_country:
+    :param via_country:
+    :param countries_contents:
+    :return:
+    """
+    from_country = entry.get("from").get("country")
+
+    if countries_contents[from_country].get("medical_advisory") != "":
+        return True
+
+    else:
+        if entry.get("via") is not None:
+            via_country = entry.get("via").get("country")
+            if countries_contents[via_country].get("medical_advisory") != "":
+                return True
+
+    return False
+
+
+def check_record_completeness(entry):
+    """
+
+    :param entry:
+    :return: returns true if reject, false if passed
+    """
+    valid = True
+
+    if not valid_passport_format(entry.get("passport")):
+        valid = False
+
+    for locations in entry.get("home"):
+        if entry.get("home")[locations] == "":
+            valid = False
+    for places in entry.get("from"):
+        if entry.get("from")[places] == "":
+            valid = False
+    for key in entry:
+        if entry[key] == "":
+            valid = False
+
+    if valid is False:
+        return True
+
+    return False
 
 def valid_date_check(issue_date):
     """
@@ -177,3 +221,5 @@ def valid_date_format(date_string):
         return True
     except ValueError:
         return False
+
+
